d227388aab471a021f4f61fff966f708b9a28c7b
akhlesh-malav <36731192+akhlesh-malav@users.noreply.github.com>
2018-03-14 14:56:25 +0530

    Update App.java
    updated

--- src/main/java/ProjectLearnGit/ProjectLearnGit/App.java
+++ src/main/java/ProjectLearnGit/ProjectLearnGit/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello GitHub!" );
+        System.out.println( "Hello GitHub For Learning here!" );
      
     }
 }
